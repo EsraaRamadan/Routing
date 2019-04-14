88320b8561029b2edb0f0dcd04ec18e860ff06e7
EsraaRamadan <esraa.r.rashad@gmail.com>
2019-04-14 17:02:18 +0200

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,8 @@ import { ServersService } from './servers/servers.service';
 const appRoutes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'users', component: UsersComponent},
+  { path: 'users/:id', component: UserComponent},
+  { path: 'users/:id/:name', component: UserComponent},
   { path: 'servers', component: ServersComponent},
 ];
 
